6e112fcc74a0100be31606cc02f8c5d2690b9998
Lape20 <laurodriguez@frba.utn.edu.ar>
2026-04-10 01:14:29 +0000

    Terminado el tp 0

---
+++ client/client.c
@@ -0,0 +1,136 @@
+#include "client.h"
+#include "utils.h"
+#include <readline/readline.h>
+
+int main(void)
+{
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	t_log* logger;
+	t_config* config;
+
+	/* ---------------- LOGGING ---------------- */
+
+	logger = iniciar_logger();
+	log_info(logger, "Hola! Soy un log");
+
+
+	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
+
+	config = iniciar_config();
+
+	if(config == NULL){
+		printf("No se pudo crear el config!");
+		abort();
+	}
+
+	// Usando el config creado previamente, leemos los valores del config y los
+	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
+
+	// Loggeamos el valor de config
+	log_info(logger, valor);
+
+
+	/* ---------------- LEER DE CONSOLA ---------------- */
+
+	leer_consola(logger);
+
+	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+
+	// Creamos una conexión hacia el servidor
+	conexion = crear_conexion(ip, puerto);
+
+	// Enviamos al servidor un mensaje handshake
+	if(enviar_handshake(conexion) != 0) {
+		printf("Error en el handshake con el servidor\n");
+		abort();
+	} else {
+		printf("handshake exitoso con el servidor");
+	}
+
+	// Enviamos al servidor el valor de CLAVE como mensaje
+
+	enviar_mensaje(valor, conexion);
+
+	// Armamos y enviamos el paquete
+	paquete(conexion);
+
+	terminar_programa(conexion, logger, config);
+
+	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
+	// Proximamente
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger = log_create("tp0.log", "tp0", true, LOG_LEVEL_INFO);
+
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config = config_create("cliente.config");
+
+	return nuevo_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	// La primera te la dejo de yapa
+	leido = readline("> ");
+
+	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+
+	while(strcmp(leido, "") != 0) {
+		log_info(logger, leido);
+		free(leido);
+		leido = readline("> ");
+	}
+	// ¡No te olvides de liberar las lineas antes de regresar!
+	free(leido);
+
+}
+
+void paquete(int conexion)
+{
+	// Ahora toca lo divertido!
+	char* leido;
+	t_paquete* paquete;
+
+	// Leemos y esta vez agregamos las lineas al paquete
+	leido = readline("> ");
+	while(strcmp(leido, "") != 0){
+		paquete = crear_paquete();
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		enviar_paquete(paquete, conexion);
+		eliminar_paquete(paquete);
+		free(leido);
+		leido = readline("> ");
+	}
+	free(leido);
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config)
+	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	liberar_conexion(conexion);
+	log_destroy(logger);
+	config_destroy(config);
+}
---
+++ server/utils (1).c
@@ -0,0 +1,121 @@
+#include"utils.h"
+
+t_log* logger;
+
+int iniciar_servidor(void)
+{
+	// Quitar esta línea cuando hayamos terminado de implementar la funcion
+	//assert(!"no implementado!");
+
+	int socket_servidor;
+
+	struct addrinfo hints, *servinfo, *p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+
+	// Creamos el socket de escucha del servidor
+	
+	socket_servidor = socket(servinfo->ai_family,
+							 servinfo->ai_socktype,
+							 servinfo->ai_protocol);
+
+	// Asociamos el socket a un puerto
+	int opt = 1;
+	setsockopt(socket_servidor, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	// Escuchamos las conexiones entrantes
+
+	listen(socket_servidor, SOMAXCONN);
+
+	freeaddrinfo(servinfo);
+	log_trace(logger, "Listo para escuchar a mi cliente");
+
+	return socket_servidor;
+}
+
+int esperar_cliente(int socket_servidor)
+{
+	// Quitar esta línea cuando hayamos terminado de implementar la funcion
+	//assert(!"no implementado!");
+
+	// Aceptamos un nuevo cliente
+	int socket_cliente;
+	socket_cliente = accept(socket_servidor, NULL, NULL);
+	log_info(logger, "Se conecto un cliente!");
+
+	return socket_cliente;
+}
+
+void recibir_handshake(int socket_cliente) {
+	size_t bytes;
+
+	int32_t handshake;
+	int32_t resultOk = 0;
+	int32_t resultError = -1;
+
+	bytes = recv(socket_cliente , &handshake, sizeof(int32_t), MSG_WAITALL);
+	if (handshake == 1) {
+    	bytes = send(socket_cliente, &resultOk, sizeof(int32_t), 0);
+	} else {
+    	bytes = send(socket_cliente, &resultError, sizeof(int32_t), 0);
+	}
+}
+
+int recibir_operacion(int socket_cliente)
+{
+	int cod_op;
+	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+		return cod_op;
+	else
+	{
+		close(socket_cliente);
+		return -1;
+	}
+}
+
+void* recibir_buffer(int* size, int socket_cliente)
+{
+	void * buffer;
+
+	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
+	buffer = malloc(*size);
+	recv(socket_cliente, buffer, *size, MSG_WAITALL);
+
+	return buffer;
+}
+
+void recibir_mensaje(int socket_cliente)
+{
+	int size;
+	char* buffer = recibir_buffer(&size, socket_cliente);
+	log_info(logger, "Me llego el mensaje %s", buffer);
+	free(buffer);
+}
+
+t_list* recibir_paquete(int socket_cliente)
+{
+	int size;
+	int desplazamiento = 0;
+	void * buffer;
+	t_list* valores = list_create();
+	int tamanio;
+
+	buffer = recibir_buffer(&size, socket_cliente);
+	while(desplazamiento < size)
+	{
+		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
+		desplazamiento+=sizeof(int);
+		char* valor = malloc(tamanio);
+		memcpy(valor, buffer+desplazamiento, tamanio);
+		desplazamiento+=tamanio;
+		list_add(valores, valor);
+	}
+	free(buffer);
+	return valores;
+}
